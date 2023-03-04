a388e82db1b1b06fb7c2435be7c68f052d16dee4
maxilar20 <dorne99@hotmail.com>
2023-03-04 11:41:39 +0000

    Added robot pick

--- src/baxter_chess_game/scripts/chess_game.py
+++ src/baxter_chess_game/scripts/chess_game.py
@@ -21,10 +21,91 @@ from gazebo_msgs.srv import (
     DeleteModel,
 )
 
+import baxter_interface
+import moveit_commander
 
 class ChessGame(object):
-    def __init__(self, verbose=True):
-        self.piece_positions = [(1,1)]
+    def __init__(self, limb, hover_distance=0.15, verbose=True):
+        self._limb_name = limb  # string
+        self._hover_distance = hover_distance  # in meters
+        self._verbose = verbose  # bool
+        self._limb = baxter_interface.Limb(limb)
+        self._gripper = baxter_interface.Gripper(limb)
+        print("Getting robot state... ")
+        self._rs = baxter_interface.RobotEnable(baxter_interface.CHECK_VERSION)
+        self._init_state = self._rs.state().enabled
+        print("Enabling robot... ")
+
+        self._robot = moveit_commander.RobotCommander()
+        self._group = moveit_commander.MoveGroupCommander(limb+"_arm")
+
+    def move_to_start(self, start_angles=None):
+        print("Moving the {0} arm to start pose...".format(self._limb_name))
+
+        self.gripper_open()
+        self._group.set_pose_target(start_angles)
+        plan = self._group.plan()
+        self._group.execute(plan)
+        rospy.sleep(1.0)
+        print("Running. Ctrl-c to quit")
+
+    def _guarded_move_to_joint_position(self, joint_angles):
+        if joint_angles:
+            self._limb.move_to_joint_positions(joint_angles)
+        else:
+            rospy.logerr("No Joint Angles provided for move_to_joint_positions. Staying put.")
+
+    def gripper_open(self):
+        self._gripper.open()
+        rospy.sleep(1.0)
+
+    def gripper_close(self):
+        self._gripper.close()
+        rospy.sleep(1.0)
+
+    def _approach(self, pose):
+        approach = copy.deepcopy(pose)
+        approach.position.z = approach.position.z + self._hover_distance
+
+        self._group.set_pose_target(approach)
+        plan = self._group.plan()
+        self._group.execute(plan)
+
+    def _retract(self):
+        # retrieve current pose from endpoint
+        current_pose = self._limb.endpoint_pose()
+        ik_pose = Pose()
+        ik_pose.position.x = current_pose['position'].x
+        ik_pose.position.y = current_pose['position'].y
+        ik_pose.position.z = current_pose['position'].z + self._hover_distance
+        ik_pose.orientation.x = current_pose['orientation'].x
+        ik_pose.orientation.y = current_pose['orientation'].y
+        ik_pose.orientation.z = current_pose['orientation'].z
+        ik_pose.orientation.w = current_pose['orientation'].w
+
+        # servo up from current pose
+        self._group.set_pose_target(ik_pose)
+        plan = self._group.plan()
+        self._group.execute(plan)
+
+    def _servo_to_pose(self, pose):
+        # servo down to release
+        self._group.set_pose_target(pose)
+        plan = self._group.plan()
+        self._group.execute(plan)
+
+    def pick(self, pose):
+        self.gripper_open()
+        self._approach(pose)
+        self._servo_to_pose(pose)
+        self.gripper_close()
+        self._retract()
+
+    def place(self, pose):
+        self._approach(pose)
+        self._servo_to_pose(pose)
+        self.gripper_open()
+        self._retract()
 
 def load_gazebo_models():
     rospy.wait_for_service("gazebo/spawn_sdf_model")
@@ -112,10 +193,28 @@ def main():
     load_gazebo_models()
     rospy.on_shutdown(delete_gazebo_models)
 
-    chess_game = ChessGame()
+    rospy.wait_for_message("/robot/sim/started", Empty)
+
+    limb = 'left'
+    hover_distance = 0.15  # meters
+
+    overhead_orientation = Quaternion(x=-0.0249590815779, y=0.999649402929, z=0.00737916180073, w=0.00486450832011)
+
+    
+    chess_game = ChessGame(limb, hover_distance)
+
+
+    starting_pose = Pose(
+        position=Point(x=0.7, y=0.135, z=0.35),
+        orientation=overhead_orientation)
+    
+    chess_game.move_to_start(starting_pose)
 
     while not rospy.is_shutdown():
-        print("\n "+str(chess_game.piece_positions))
+        print("\nPicking...")
+        piece_pos = rospy.get_param('piece_target_position_map')["01"]
+        block_pose = Pose(position = Point(x=piece_pos[0],y=piece_pos[1],z=piece_pos[2]), orientation = overhead_orientation)
+        chess_game.pick(block_pose)
     return 0
 
 if __name__ == '__main__':
